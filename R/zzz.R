495a93cf85e1369d0e12559f35102a672ce7716b
But-I-Digress <charleshschid@gmail.com>
2018-09-17 09:52:11 -0400

    Minor code tweak.

--- R/zzz.R
+++ R/zzz.R
@@ -5,13 +5,11 @@ xlsxSimple <- new.env()
 
 .onLoad <- function(libname, pkgname) {
 	# The xlsx package sets these options so let's save the values and then put them back after xlsx is loaded. 
-	xlsx.datetime.format <- getOption("xlsx.datetime.format")
-	if (is.null(xlsx.datetime.format)) xlsx.datetime.format <- "d MMM yyyy"
-	xlsx.date.format <- getOption("xlsx.date.format")
-	if (is.null(xlsx.date.format)) xlsx.date.format <- "d MMM yyyy"
+	xlsx.datetime.format <- getOption("xlsx.datetime.format",  "d MMM yyyy")
+	xlsx.date.format <- getOption("xlsx.date.format",  "d MMM yyyy")
 	
-	#Create a workbook and store it. This will allow xlsx to do its initialization. 
-	assign("book", xlsx::createWorkbook(), envir=xlsxSimple)
+	# Create a workbook and store it. This will allow xlsx to do its initialization. 
+	assign("book", xlsx::createWorkbook(), envir = xlsxSimple)
 	
 	options(xlsx.datetime.format = xlsx.datetime.format, xlsx.date.format = xlsx.date.format)
 }
